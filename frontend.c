0609c1bfa88d40bdd4b661883834db5e3d3b8ba4
Leonardo Henrique <sellionrb@outlook.com>
2018-10-26 14:51:13 -0300

    Add files via upload

--- frontend.c
+++ frontend.c
@@ -4,29 +4,32 @@
 #include "frontend.h"
 
 void ler_arquivo(struct matriz* m, char* fileName){
-    FILE* fp;
-    fp = fopen("C:\\Users\\selli\\Desktop\\arquivoES_TP2.txt");
 
-    if (fp==NULL)
-    {
-        printf("Erro");
-        return 0;
-    }
-    int Linha[100];
-    int num = 0, cont;
+        char fp[] = "C:\\Users\\selli\\Desktop\\arquivoES_TP2.txt",
+                ch;
+        int i, posicao = 0, matriculaconv[13];
+        char matricula[13];
 
-    while (!feof(fp))
-    {
-        fgets(Linha, 100, fp);
-            printf("%d",Linha);
-    }
-    while( (cont=fgetc(arq))!= EOF )
-    {
-        if(cont == '\n')
-			  num++;
-    }
 
-	printf("Existem %d linhas no arquivo\n", num);
-    fclose(fp);
-    return;
+        FILE *arq;
+
+        arq = fopen(fp, "r");
+        if (arq == NULL)
+            printf("Erro, nao foi possivel abrir o arquivo\n");
+
+        else {
+            while (((fscanf(arq, "%c", &matricula[posicao])) != EOF) || (posicao <= 13)) {
+                posicao++;
+            }
+
+            for (i = 0; i <= 13; i++) {
+                printf("%c \n", matricula[i]);
+            }
+        }
+        for(i=0;i<=13;i++){
+            matriculaconv[i]=(matricula[i]-'0');
+            printf("%d", matriculaconv[i]);
+        }
 }
+
+
--- frontend.h
+++ frontend.h
@@ -1,12 +1,11 @@
 #ifndef _TRAVELING_FRONTEND_H
 #define _TRAVELING_FRONTEND_H
-
 /**
  * Le as 3 matriculas
  * e o 'N'
  */
 void interativo();
 
-void lerArquivo(char *fileName);
+void ler_arquivo(struct matriz* m, char* fileName);
 
 #endif
