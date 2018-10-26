02633bf41753a32bf2def12b9f0c49499f97ebcd
Leonardo Henrique <sellionrb@outlook.com>
2018-10-26 10:28:19 -0300

    Add files via upload

--- frontend.c
+++ frontend.c
@@ -1,2 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "controller.h"
 #include "frontend.h"
+
+void ler_arquivo(struct matriz* m, char* fileName){
+    FILE* fp;
+    fp = fopen("C:\\Users\\selli\\Desktop\\arquivoES_TP2.txt");
+
+    if (fp==NULL)
+    {
+        printf("Erro");
+        return 0;
+    }
+    int Linha[100];
+    int num = 0, cont;
+
+    while (!feof(fp))
+    {
+        fgets(Linha, 100, fp);
+            printf("%d",Linha);
+    }
+    while( (cont=fgetc(arq))!= EOF )
+    {
+        if(cont == '\n')
+			  num++;
+    }
+
+	printf("Existem %d linhas no arquivo\n", num);
+    fclose(fp);
+    return;
+}
